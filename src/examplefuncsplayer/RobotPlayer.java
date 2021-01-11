4bb25489e4f108477d6b8d387ee35dceee503b46
yuyeon <li.alan180@gmail.com>
2021-01-10 18:48:22 -0800

    added encodeLocation and getLocation

--- src/examplefuncsplayer/RobotPlayer.java
+++ src/examplefuncsplayer/RobotPlayer.java
@@ -141,4 +141,60 @@ public strictfp class RobotPlayer {
             return true;
         } else return false;
     }
+
+    /*
+    Encodes location into flag, using the rightmost 14 bits. The remaining leftmost bits are used to encode any
+    other information.
+    */
+    static void sendLocation(int extraInfo) throws GameActionException {
+        MapLocation loc = rc.getLocation();
+        int x = loc.x, y = loc.y;
+        int encodedLocation = ((x % 128) * 128) + (y % 28) + (extraInfo * 128 * 128);
+        if(rc.canSetFlag(encodedLocation)){
+            rc.setFlag(encodedLocation);
+        }
+    }
+
+    static final int BITS = 7;
+
+    /*
+    Decodes location from given flag.
+    */
+    static MapLocation getLocation(int flag) throws GameActionException{
+        int y = flag & BITS;
+        int x = (flag >> BITS) & 127;
+
+        MapLocation currLoc = rc.getLocation();
+
+        int relX = currLoc.x % 128;
+        int relY = currLoc.y % 128;
+
+        int actualX = 0, actualY = 0;
+
+        int xDiff = relX - x;
+
+        if(Math.abs(xDiff) < 64){
+            actualX = ((currLoc.x >> BITS) << BITS) + x;
+        }
+        else if(xDiff >= 64){
+            actualX = ((currLoc.x >> BITS) << BITS) + 128 + x;
+        }
+        else if(xDiff <= -64){
+            actualX = ((currLoc.x >> BITS) << BITS) - 128 + x;
+        }
+
+        int yDiff = relY - y;
+
+        if(Math.abs(yDiff) < 64){
+            actualY = ((currLoc.y >> BITS) << BITS) + y;
+        }
+        else if(yDiff >= 64){
+            actualY = ((currLoc.y >> BITS) << BITS) + 128 + y;
+        }
+        else if(yDiff <= -64){
+            actualY = ((currLoc.y >> BITS) << BITS) - 128 + y;
+        }
+
+        return new MapLocation(actualX, actualY);
+    }
 }
